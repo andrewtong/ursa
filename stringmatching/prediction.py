3c73ba1a7f245643a201347f017bb1a30b8c7bdf
Andrew Tong <andrewtong19@gmail.com>
2015-12-05 18:43:36 -0800

    Update prediction.py

--- stringmatching/prediction.py
+++ stringmatching/prediction.py
@@ -36,77 +36,83 @@ def compare(main, substring):
         return
     else:
         print('No matches were found to ' + substring + ' with reasonable certainty.')
-        return   
- 
+        return         
+        
 def score(main, substring):
     #score functions identically to compare, except it returns the integer value that represents the likeliness between
     #the two strings as opposed to a string value.
-    main = filterstringpattern(main)
+    main = filter_string_pattern(main)
     for k in range(0, len(substring)):
-        dividedlist = divlist(main, substring[k], len(substring))
-        scores = [k*10]*len(dividedlist)
-        result = checklist(dividedlist, substring[k:], 0, scores)
+        divided_list = div_list(main, substring[k], len(substring))
+        scores = [k*10]*len(divided_list)
+        result = check_list(divided_list, substring[k:], 0, scores)
         for key,v in result.items():
-            if key is not None:
-                if int(v) > 25:
-                    adjustedscore = -0.1*abs((int(v)-100))**1.6 + 100
-                    #Adjusted score is the score that better represents the accuracy of the predicted matched word.
-                    #The purpose of using the adjusted score is that the inverse exponential portion makes the scoring such that
-                    #lower and higher end scores are more likely to be output, which give a definitive answer, while
-                    #medium-ranged scores that are not exactly very informative of how accurate the word is are distributed
-                    #less often.
-                    return int(adjustedscore)
+            if key is not None and int(v) > 25:
+                adjusted_score = -0.1*abs((int(v)-100))**1.6 + 100
+                #Adjusted score is the score that better represents the accuracy of the predicted matched word.
+                #The purpose of using the adjusted score is that the inverse exponential portion makes the scoring such that
+                #lower and higher end scores are more likely to be output, which give a definitive answer, while
+                #medium-ranged scores that are not exactly very informative of how accurate the word is are distributed
+                #less often.
+                return int(adjusted_score)
         if k > int(len(substring)/2):
-            return 0      
+            return 0
+     
        
 #This function filters out irrelevant characters from the string pattern
-def filterstringpattern(strg):
+def filter_string_pattern(strg):
     words = sub(r'[^\w\s]','',strg) #Remove all punctuation
     words = words.replace(' ','') #Removes spaces between words
     words = words.lower()  #Turn all capital letters to lower letters
     return words
 
-#divlist will return a list with all sets of words that start with delimiter 'delim'
-#It is important to know that all of the elements return by divlist will have a minimum length equal 
+#div_list() will return a list with all sets of words that start with delimiter 'delim'
+#It is important to know that all of the elements return by div_list() will have a minimum length equal 
 #to the length of the substring.  This is done using a neat partitioning trick that works almost in linear
 #time by using a queue.
-def divlist(strg, delim, sslen): #assumes string is already filtered
+
+#Notation
+#string_pat: String Pattern
+#delim: delimiting letter of the substring
+#ss_len: substring length
+
+def div_list(string_pat, delim, ss_len): #assumes string is already filtered
     q = []
-    splitlist = []
-    dividedlist = []
-    for ch in strg:
+    split_list = []
+    divided_list = []
+    for ch in string_pat:
         if ch not in delim:
-            splitlist.append(ch)
+            split_list.append(ch)
         else:
-            if splitlist:
-                templength = abs(len(splitlist) - sslen)
-                if splitlist[0] == delim:
-                    if len(splitlist) < sslen:
-                        q.append(templength)
-                        templength = 0
-                    dividedlist.append(''.join(splitlist))
-            splitlist = []
-            splitlist.append(ch)
+            if split_list:
+                temp_length = abs(len(split_list) - ss_len)
+                if split_list[0] == delim and len(split_list) < ss_len:
+                    #if len(split_list) < ss_len:
+                    q.append(temp_length)
+                    temp_length = 0
+                    divided_list.append(''.join(split_list))
+            split_list = []
+            split_list.append(ch)
         if q:
             for i in range (1,len(q)+1): #not sure if this can be made into a comprehension
-                dividedlist[-i] += ch
+                divided_list[-i] += ch
             q = [elem - 1 for elem in q]
             if q[0] is 0:
                 q.pop(0)
-    if splitlist:
-        dividedlist.append(''.join(splitlist))
-    return dividedlist
+    if split_list:
+        divided_list.append(''.join(split_list))
+    return divided_list
 
 #This is the basic function to perform a preliminary check to see whether the partitioned words
 #may possibly be the 'correct' word.  If it is, it is sent to a secondary check which singles out the 
 #potential word.
-def checklist(dividedlist, substr, index, scores):
+def check_list(divided_list, substr, index, scores):
     rating = {}
-    dlistlen = len(dividedlist)
+    div_list_len = len(divided_list)
     #[obj, obj, obj, obj]
     #[ s1, s2 , s3 , s4 ]
     
-    for i in range(0,dlistlen):
+    for i in range(0,div_list_len):
         
         #If this function is called and the index is greater than the length of the substring, this will
         #indicate there is no answer.  This will occur during latter letter partitions in complex cases.            
@@ -127,126 +133,131 @@ def checklist(dividedlist, substr, index, scores):
             #or two positions forward from the index, and the algorithm will be able to recognize that there 
             #is a character match.  That being said, the further off the character is off from the original
             #position, the more points it earns toward the word being 'incorrect'.
-            searchdist = 3 #, len(substr)-index)
+            search_dist = 3 #, len(substr)-index)
 
             #if there is no direct letter match at the same index
-            if dividedlist[i][index] is substr[index]: 
+            if divided_list[i][index] is substr[index]: 
                 scores[i] += 0
             #if two letters are correct but just swapped, the algorithm swaps them back.
-            elif index < len(dividedlist[i]) - 1 and substr[index + 1] is dividedlist[i][index] and dividedlist[i][index + 1] is substr[index]:
-                dividedlistl = list(dividedlist[i])
-                dividedlistl[index + 1], dividedlistl[index] = dividedlistl[index], dividedlistl[index + 1]
-                dividedlist[i] = ''.join(dividedlistl)
+            elif index < len(divided_list[i]) - 1 and substr[index + 1] is divided_list[i][index] and divided_list[i][index + 1] is substr[index]:
+                divided_listl = list(divided_list[i])
+                divided_listl[index + 1], divided_listl[index] = divided_listl[index], divided_listl[index + 1]
+                divided_list[i] = ''.join(divided_listl)
                 scores[i] += 10
             #if a letter is out of place, letter insertions/deletions are performed.
-            elif substr[index + 1] in dividedlist[i][index:index+searchdist+1]:
-                nextletterindex = dividedlist[i][index:index+searchdist+1].index(substr[index + 1]) + index
+            elif substr[index + 1] in divided_list[i][index:index+search_dist+1]:
+                next_letter_index = divided_list[i][index:index+search_dist+1].index(substr[index + 1]) + index
                 #case of a one letter off 
-                if nextletterindex < index + 1:
-                    dividedlistl = list(dividedlist[i])
-                    dividedlistl.insert(index,substr[index])
-                    dividedlist[i] = ''.join(dividedlistl)
+                if next_letter_index < index + 1:
+                    divided_listl = list(divided_list[i])
+                    divided_listl.insert(index,substr[index])
+                    divided_list[i] = ''.join(divided_listl)
                     scores[i] += 15
                 #case where there are multiple letters on the index
-                elif nextletterindex >= index + 1:
-                    dividedlist[i] = dividedlist[i].replace(dividedlist[i][index:nextletterindex],substr[index])
-                    scores[i] += 10 + 3**((nextletterindex - (index + 1)) + 1)
+                elif next_letter_index >= index + 1:
+                    divided_list[i] = divided_list[i].replace(divided_list[i][index:next_letter_index],substr[index])
+                    scores[i] += 10 + 3**((next_letter_index - (index + 1)) + 1)
 
             else: #if word does not match, and is not one over within a margin of 3
                 #likely indicates that the current and next letter are off, so the word needs to check
                 #the following letter to see if it is correct
-                dividedlistl = list(dividedlist[i])
-                dividedlistl.insert(index,substr[index])
-                dividedlist[i] = ''.join(dividedlistl)
+                divided_listl = list(divided_list[i])
+                divided_listl.insert(index,substr[index])
+                divided_list[i] = ''.join(divided_listl)
                 scores[i] += 35
         except IndexError:
             #string pattern length is smaller than the length of the substring
-            dividedlistl = list(dividedlist[i])
-            dividedlistl.insert(index,substr[index])
-            dividedlist[i] = ''.join(dividedlistl)
+            divided_listl = list(divided_list[i])
+            divided_listl.insert(index,substr[index])
+            divided_list[i] = ''.join(divided_listl)
             scores[i] += 15
         
         #Once every word in dlistlen is checked, the scores of all elements are checked.  Any scores
         #that are reasonable are further checked, while poor scores are thrown out.
-        if i is dlistlen-1:  
-            #checks uncertainty of the word
-            for idx, score in enumerate(scores):
-                #if it is bad, then you throw the word out from the dividedlist
-                if score > len(substr)*10:
-                    dividedlist.pop(idx)
-                    scores.pop(idx)
-                if score < threshold:
-                    #check accuracy should return a dictionary with the 'word' and its accuracy
-                    #add the dictionary to the dictionary rating
-                    dictresult = checkaccuracy(dividedlist[idx], substr, index + 1, score)
-                    if rating:
-                        maxword = max(rating, key=(lambda key: rating[key]))
-                        maxscre = rating[maxword]
-                        for k,v in dictresult.items():
-                            if v > maxscre:
-                                rating.update(dictresult)
-                    else:
-                        rating.update(dictresult)
-                    #sort the dictionary by highest rating its size is > 1
-                    #return the highest value from the dictionary
-            if rating:
-                word = max(rating, key=(lambda key: rating[key]))
-                scre = rating[word]
-                return {word:scre}
-            
-            #If there are no more possible matches, the algorithm knows that partitions at this letter have
-            #no correct matches.
-            if not dividedlist:
-                return {None: 0}
-            
-            #If there a no satisfactory scores, the algorithm repeats at the following index.
-            return checklist(dividedlist, substr, index + 1, scores)
+        if i is div_list_len-1:
+            return search_highest_match(scores, substr, threshold, divided_list, rating, index)  
+
         
         #This is the end condition where no possible matches are found.  Happens in extreme cases where
         #the string pattern is very small in length (e.x. 2 characters).
-        if i == dlistlen and index == len(substr):
+        if i == div_list_len and index == len(substr):
             return {None: 0}
+        
+def search_highest_match(scores, substr, threshold, divided_list, rating, index):
+    #checks uncertainty of the word
+    for idx, score in enumerate(scores):
+        #if it is bad, then you throw the word out from the dividedlist
+        if score > len(substr)*10:
+            divided_list.pop(idx)
+            scores.pop(idx)
+        if score < threshold:
+            #check accuracy should return a dictionary with the 'word' and its accuracy
+            #add the dictionary to the dictionary rating
+            dict_result = check_accuracy(divided_list[idx], substr, index + 1, score)
+            if rating:
+                max_word = max(rating, key=(lambda key: rating[key]))
+                max_score = rating[max_word]
+                for k,v in dict_result.items():
+                    if v > max_score:
+                        rating.update(dict_result)
+            else:
+                rating.update(dict_result)
+            #sort the dictionary by highest rating its size is > 1
+            #return the highest value from the dictionary
+    if rating:
+        word = max(rating, key=(lambda key: rating[key]))
+        scre = rating[word]
+        return {word:scre}
+    
+    #If there are no more possible matches, the algorithm knows that partitions at this letter have
+    #no correct matches.
+    if not divided_list:
+        return {None: 0}
+    
+    #If there a no satisfactory scores, the algorithm repeats at the following index.
+    return check_list(divided_list, substr, index + 1, scores)
+    
 
 #This performs a similar function to checklist except it checks a single element against the substring.  
 #This is used when the algorithm is fairly certain there may be a match, and will return a rating from
 #0-100 depending on the accuracy.  See the checklist function for greater detail on how this function operates.
-def checkaccuracy(words, substr, index, score):
+def check_accuracy(words, substr, index, score):
     
     for i in range(index, len(substr)):
         try:
-            searchdist = 3
+            search_dist = 3
             #if there is no direct letter match at the same index
             if words[i] is substr[i]: 
                 #score add 0 
                 score += 0
             elif i < len(words) - 1 and substr[i + 1] is words[i] and words[i + 1] is substr[i]:
-                wordsl = list(words)
-                wordsl[i + 1], wordsl[i] = wordsl[i], wordsl[i + 1]
-                words = ''.join(wordsl)
+                words_list = list(words)
+                words_list[i + 1], words_list[i] = words_list[i], words_list[i + 1]
+                words = ''.join(words_list)
                 score += 10
-            elif substr[i + 1] in words[i:i+searchdist+1]:
-                nextletterindex = words[i:i+searchdist+1].index(substr[i + 1]) + i
+            elif substr[i + 1] in words[i:i+search_dist+1]:
+                next_letter_index = words[i:i+search_dist+1].index(substr[i + 1]) + i
                 #case of a one letter off 
-                if nextletterindex < i + 1:
-                    wordsl = list(words)
-                    wordsl.insert(index,substr[i])
-                    words = ''.join(wordsl)
+                if next_letter_index < i + 1:
+                    words_list = list(words)
+                    words_list.insert(index,substr[i])
+                    words = ''.join(words_list)
                     score += 15
                 #case where there are multiple letters on the index
-                elif nextletterindex >= i + 1:
-                    words = words.replace(words[i:nextletterindex],substr[i])
-                    score += 10 + 3**((nextletterindex - (i + 1)) + 1)
+                elif next_letter_index >= i + 1:
+                    words = words.replace(words[i:next_letter_index],substr[i])
+                    score += 10 + 3**((next_letter_index - (i + 1)) + 1)
 
             else:
-                wordsl = list(words)
-                wordsl.insert(i,substr[i])
-                words = ''.join(wordsl)
+                words_list = list(words)
+                words_list.insert(i,substr[i])
+                words = ''.join(words_list)
                 score += 35
         except IndexError:
             #this means that the word is 1 letter shorter than the substring, and it hits an index error
-            wordsl = list(words)
-            wordsl.insert(i,substr[i])
-            words = ''.join(wordsl)
+            words_list = list(words)
+            words_list.insert(i,substr[i])
+            words = ''.join(words_list)
             score += 15
 
     rating = 100*(1 - score/(len(substr)*20))
@@ -254,3 +265,5 @@ def checkaccuracy(words, substr, index, score):
     matcher = words[0:len(substr)]
     return {matcher:rating}
 
+#compare('A secondary fnctin comparescore can be used to exclusively retrieve the likeliness between two strings.', 'function')
+#print(score('A secondary fnctin comparescore can be used to exclusively retrieve the likeliness between two strings.', 'function'))
